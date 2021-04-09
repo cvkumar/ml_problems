a1759a5e3e570f8bf09721c97f8b2cf06c7d9844
Caleb Kumar <caleb.kumar@hiremya.com>
2021-04-09 12:25:38 -0400

    neural network p2 as a classes to make more readable.

--- neural_network_p2.py
+++ neural_network_p2.py
@@ -8,70 +8,52 @@ import pandas as pd
 
 import math
 
-RANDOM_STATE = 12
+RANDOM_STATE = 13
 random.seed(RANDOM_STATE)
 
 df = pd.read_csv("data/seeds/wheat-seeds.csv")
 
+
 # print(df.shape)
+# Number of weights, number of neurons
+# [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
 
 
-def initialize_network(n_inputs, n_hidden, n_outputs):
-    network = []
-    # TODO: Think about these layers more
-    hidden_layer = [
-        {"weights": [random.random() for i in range(n_inputs + 1)]}
-        for i in range(n_hidden)
-    ]
-    network.append(hidden_layer)
-    output_layer = [
-        {"weights": [random.random() for i in range(n_hidden + 1)]}
-        for i in range(n_outputs)
-    ]
-    network.append(output_layer)
-    return network
+class Neuron:
+    def __init__(self, weights, bias=None):
+        self.bias = bias or random.random()
+        self.weights = weights
+
+    def __str__(self):
+        return f"weights: {self.weights}, bias: {self.bias}"
 
 
-def activate(weights, inputs):
-    activation = weights[-1] # This is the bias value
-    for i in range(len(inputs) - 1):
-        activation += weights[i] * inputs[i]
-    return activation
+class NeuralNetwork:
+    def __init__(self, n_inputs, n_hidden, n_outputs):
+        # Add a neuron for n_hidden with random weights that can handle the number of inputs
+        self.hidden_layer = [
+            Neuron(bias=random.random(), weights=self._generate_weights(n_inputs))
+            for i in range(n_hidden)
+        ]
+
+        # Add a neuron for each output with random weights that can handle the number of hidden layer neurons
+        self.output_layer = [
+            Neuron(bias=random.random(), weights=self._generate_weights(n_hidden))
+            for i in range(n_outputs)
+        ]
+
+    def _generate_weights(self, n_weights: int) -> list:
+        return [random.random() for i in range(n_weights)]
+
+    def activate(self, inputs, weights):
+        pass
+
+    def __str__(self):
+        hidden_layer = f"hidden_layer: {[str(neuron) for neuron in self.hidden_layer]}"
+        output_layer = f"output_layer: {[str(neuron) for neuron in self.output_layer]}"
+        return f"{hidden_layer}\n{output_layer}"
 
 
-def transfer_neuron_activation(activation):
-    # SIGMOID
-    return 1.0 / (1.0 + math.exp(activation))
+network = NeuralNetwork(2, 1, 2)
 
-
-def forward_propagate(network, inputs):
-    """
-    given inputs
-
-    """
-    # TODO: There's a bug here to fix
-    layer_values = inputs.copy()
-    for layer in network:
-        temp = []
-        for neuron in layer:
-            activation_val = transfer_neuron_activation(activate(weights=neuron['weights'], inputs=layer_values))
-            temp.append(activation_val)
-
-        layer_values = temp
-
-    return layer_values
-
-
-network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
-		[{'weights': [0.2550690257394217, 0.49543508709194095]}, {'weights': [0.4494910647887381, 0.651592972722763]}]]
-
-row = [1, 0, None]  # last one is bias
-output = forward_propagate(network, row)
-print(output)
-
-
-
-# activation = sum(weight_i * input_i) + bias
-
-# for layer in network:
-#     print(layer)
+print(network)
