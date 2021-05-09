928fa0e4dfb86e593262acf374908cfe69ece489
Caleb Kumar <caleb.kumar@hiremya.com>
2021-05-09 10:49:14 -0400

    print most common words for BarracudaNetworksMLTakeHomeQuestions

--- barracuda_networks/data_retriever.py
+++ barracuda_networks/data_retriever.py
@@ -1,4 +1,3 @@
-import pandas as pd
 
 
 def print_most_common_words(n=100):
@@ -19,6 +18,19 @@ def print_most_common_words(n=100):
 
                 else:
                     word_index_to_count[word_index] = word_index_to_count[word_index] + word_count
+
+    with open("aclImdb/test/labeledBow.feat") as f:
+        for line in f:
+            word_to_counts = line.split(" ")[1:]
+            for word_to_count in word_to_counts:
+                word_index = int(word_to_count.split(":")[0])
+                word_count = int(word_to_count.split(":")[1])
+                if word_index not in word_index_to_count:
+                    word_index_to_count[word_index] = word_count
+
+                else:
+                    word_index_to_count[word_index] = word_index_to_count[word_index] + word_count
+
     temp = [{'index': k, 'count': v} for k, v in word_index_to_count.items()]
     sorted_word_index_to_count = sorted(temp, key=lambda x: x['count'], reverse=True)
     for word_index_to_count in sorted_word_index_to_count[0:n]:
